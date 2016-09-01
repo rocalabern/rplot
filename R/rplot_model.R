77b43001d7a29bfc1feef7867e24214f2f35044b
Roc Alabern <roc.alabern@gmail.com>
2016-09-01 12:45:29 +0200

    .

--- R/rplot_model.R
+++ R/rplot_model.R
@@ -171,16 +171,14 @@ r.plot.lift <- function(
   score,
   target, 
   npoints = 20, 
-  icol = 1, col = NULL,
+  icol = 1, 
+  col = r.color(icol),
   main = "Lift Curve", 
   sub = NULL,
   showMessage = TRUE,
   target_value = NULL,
   ...)
 {
-  if(missing(col) || is.null(col)) rcolor = r.color(icol)
-  else rcolor = col
-
   data = rmodel::r.gains(score, target, npoints=npoints, mode="rnd", target_value=target_value)
   ind = data$perc>0
   x = data$perc[ind]
@@ -195,7 +193,7 @@ r.plot.lift <- function(
   
   r.plot.new(xlim=c(0,1), ylim=c(0,max(2,y)), main=main, sub=sub, ...)
   r.plot.add(x=data$perc, y=1+numeric(length(data$perc)), col=rgb(0,0,0,0.8), type="l")
-  r.plot.add(x, y, col=rcolor, type="l")
+  r.plot.add(x, y, col=col, type="l")
 }
 
 #' @title r.plot.burbujas
@@ -363,14 +361,17 @@ r.plot.gain <- function(
   mode = "def", 
   main = "Gain Curve", 
   sub=NULL,
-  icol = 1, col = NULL,
+  icol = 1, 
+  icol.max = 11,
+  col = r.color(icol),
+  col.max = r.color(icol.max),
   showMessage = TRUE,
+  showMax = TRUE,
   target_value = NULL,
   ...)
 {
-  
-  if(missing(col) || is.null(col)) rcolor = r.color(icol)
-  else rcolor = col
+  target_value_max = 1
+  if (!is.null(target_value)) target_value_max = target_value
   
   if (mode=="area") {
     dataPos = rmodel::r.gains(score, target, npoints=npoints, mode="pos", target_value=target_value)
@@ -387,11 +388,11 @@ r.plot.gain <- function(
     
     r.plot.new(x=dataPos$perc, y=dataPos$perc, main=main, sub=sub, ...)
     r.plot.add(x=dataPos$perc, y=dataPos$perc, col=rgb(0,0,0,0.8), type="l")      
-    r.plot.add(x=dataPos$perc, y=dataPos$gain, col=rcolor, type="l")
-    r.plot.add(x=dataPos$perc, y=dataNeg$gain, col=rcolor, type="l")
+    r.plot.add(x=dataPos$perc, y=dataPos$gain, col=col, type="l")
+    r.plot.add(x=dataPos$perc, y=dataNeg$gain, col=col, type="l")
     polygon(c(dataNeg$perc, rev(dataPos$perc)),
             c(dataNeg$gain, rev(dataPos$gain)),
-            col=rcolor,  border = NA)
+            col=col,  border = NA)
   } else {
     data = rmodel::r.gains(score, target, npoints=npoints, mode=mode, target_value=target_value)
     AUC = rmodel::r.auc(data$perc, data$gain)
@@ -405,7 +406,8 @@ r.plot.gain <- function(
     }
     r.plot.new(x=data$perc, y=data$perc, main=main, sub=sub, ...)
     r.plot.add(x=data$perc, y=data$perc, col=rgb(0,0,0,0.8), type="l") 
-    r.plot.add(x=data$perc, y=data$gain, col=rcolor, type="l")
+    if(showMax) r.plot.add(c(0,length(which(target==target_value_max))/(length(target)),1),c(0,1,1), col=col.max, type="l")
+    r.plot.add(x=data$perc, y=data$gain, col=col, type="l")
   }
   invisible(AUC)
 }
@@ -473,12 +475,18 @@ r.plot.gains <- function(
   sub = NULL,
   icol.train = 1,
   icol.test = 2,
+  icol.max = 11,
   col.train = r.color(icol.train),
   col.test = r.color(icol.test),
+  col.max = r.color(icol.max),
   showMessage = TRUE,
+  showMax = TRUE,
   target_value = NULL,
   ...)
 {
+  target_value_max = 1
+  if (!is.null(target_value)) target_value_max = target_value
+  
   data.train = rmodel::r.gains(score.train, target.train, npoints=npoints, mode=mode, target_value=target_value)
   AUC.train = rmodel::r.auc(data.train$perc, data.train$gain)
   
@@ -492,6 +500,7 @@ r.plot.gains <- function(
   }
   r.plot.new(c(0,1), c(0,1), main=main, sub=strAUC)
   r.plot.add(x=c(0,1), y=c(0,1), col=rgb(0,0,0,0.8), type="l")
+  if(showMax) r.plot.add(c(0,length(which(c(target.train,target.test)==target_value_max))/(length(target.train)+length(target.test)),1),c(0,1,1), col=col.max, type="l")
   r.plot.add(x=data.train$perc, y=data.train$gain, col=col.train, type="l")
   r.plot.add(x=data.test$perc, y=data.test$gain, col=col.test, type="l")
   invisible(list(AUC.train=AUC.train, AUC.test=AUC.test))
